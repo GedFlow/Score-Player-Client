97fd5cf066f11acc675f071a4fd08622c8550def
GedFlow <ptj1327@naver.com>
2024-02-12 20:08:10 +0900

    [Fix] - vercel에서 환경변수 설정장애,  MixedContent, ssl미인증 등으로 인한 버그 수정

--- src/Components/Upload.jsx
+++ src/Components/Upload.jsx
@@ -66,7 +66,7 @@ export default function Upload({setIsLoading}) {
         type="file" 
         accept="image/*" 
         id="fileInput" 
-        style={{display:"none"}}
+        style={{visibility: "hidden"}}
         onChange={inputEvent}
       />
       {
